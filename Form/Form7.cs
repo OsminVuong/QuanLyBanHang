d73e0e8315b2450c56c66cf1f32ccad955065ff1
hoang <hoang@example.com>
2018-01-06 20:25:22 +0700

    thong ke ca nhan cua nhan vien ban hang (tt)

--- Form/Form7.cs
+++ Form/Form7.cs
@@ -43,7 +43,8 @@ namespace Quanlybanhang
         {
             DemHD();
             DemHDER();
-            
+            DemSP();
+            TongDT();
         }
 
         #region====== Đếm ========
@@ -81,6 +82,42 @@ namespace Quanlybanhang
             catch (Exception)
             {
                 MessageBox.Show("Lỗi hedemx2 ", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+        }
+        private void TongDT()
+        {
+            SqlConnection Cnn = db._DbContext();
+            try
+            {
+                Cnn.Open();
+                string tamp = "SELECT sum(Tongtien) FROM [dbo].[Hoadon] Where Tongtien >0 and MaNV=@MaNV";
+                Cmd = new SqlCommand(tamp, Cnn);
+                Cmd.Parameters.Add(new SqlParameter("@MaNV", IDtext.Text));
+                int ct = int.Parse(Cmd.ExecuteScalar().ToString());
+                DTtext.Text = ct.ToString();
+                Cnn.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi hedemx2 ", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+        }
+        private void DemSP()
+        {
+            SqlConnection Cnn = db._DbContext();
+            try
+            {
+                Cnn.Open();
+                string tamp = "SELECT COUNT(DISTINCT C.MaSP) FROM Hoadon H INNER JOIN ChitietHD C ON H.MaHD = C.MaHD  Where H.Tongtien >0 and MaNV=@MaNV";
+                Cmd = new SqlCommand(tamp, Cnn);
+                Cmd.Parameters.Add(new SqlParameter("@MaNV", IDtext.Text));
+                int ct = int.Parse(Cmd.ExecuteScalar().ToString());
+                SLSP.Text = ct.ToString();
+                Cnn.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi hedemx3 ", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Question);
             }
         }
         #endregion===================================================
