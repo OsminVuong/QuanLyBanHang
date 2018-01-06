fab85913503d7851159dc0935039eef0fd4021b2
hoang <hoang@example.com>
2018-01-06 19:47:46 +0700

    Tra cuu thong tin khach hang

--- Form/Form5.cs
+++ Form/Form5.cs
@@ -14,18 +14,44 @@ namespace Quanlybanhang
 {
     public partial class Form5 : Form
     {
-        
+        #region======== Chuỗi kết nối =========
+        DbContext db = new DbContext();
+        DataSet ds = new DataSet();
+        private SqlCommand Cmd;
+        private SqlDataAdapter da = new SqlDataAdapter();
+        #endregion=============================
+
         public Form5(string input)
         {
+            MaKHtext.Text = input;
             InitializeComponent();
             
         }
         private void Form5_Load(object sender, EventArgs e)
         {
-            
+            Load_Grid();
         }
 
-        
+        private void Load_Grid()
+        {
+            SqlConnection Cnn = db._DbContext();
+            try
+            {
+                Cnn.Open();
+                string sql = "select * From ChitietKH Where MaKH=@MaKh";
+                Cmd = new SqlCommand(sql, Cnn);
+                Cmd.Parameters.Add(new SqlParameter("@MaKH", MaKHtext.Text));
+                da = new SqlDataAdapter(Cmd);
+                DataTable Table = new DataTable();
+                da.Fill(Table);
+                DataGView.AutoGenerateColumns = false;
+                DataGView.DataSource = Table;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi DataGidView");
+            }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
